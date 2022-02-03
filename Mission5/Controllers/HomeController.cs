6a0d42356df0ddd65b91b938b14f6ac8de231eea
knabrotz <keannanabrotzky@gmail.com>
2022-02-02 23:27:24 -0700

    Add project files.

---
+++ Mission5/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Mission5.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext movieContext { get; set; }
+
+        public HomeController( MovieContext x)
+        {
+            movieContext = x;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            ViewBag.Categories = movieContext.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(MovieEntry movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movieContext.Add(movie);
+                movieContext.SaveChanges();
+
+                return RedirectToAction("MovieList");
+            }
+            else
+            {
+                ViewBag.Categories = movieContext.Categories.ToList();
+                return View(movie);
+            }
+            
+        }
+
+        public IActionResult MovieList()
+        {
+            var movies = movieContext.Responses
+                .Include(x => x.Category)
+                .ToList();
+
+            return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int movieid)
+        {
+            ViewBag.Categories = movieContext.Categories.ToList();
+
+            var movie = movieContext.Responses.Single(x => x.MovieID == movieid);
+            return View("Movies", movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(MovieEntry movieEntry)
+        {
+            movieContext.Update(movieEntry);
+            movieContext.SaveChanges();
+
+            return RedirectToAction("MovieList");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int movieid)
+        {
+            var movie = movieContext.Responses.Single(x => x.MovieID == movieid);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(MovieEntry movieEntry)
+        {
+            movieContext.Responses.Remove(movieEntry);
+            movieContext.SaveChanges();
+            return RedirectToAction("MovieList");
+        }
+
+    }
+}
---
+++ Mission5/Models/MovieContext.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission5.Models
+{
+    public class MovieContext : DbContext
+    {
+        // Constructor
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+            //Leave blank
+        }
+
+        public DbSet<MovieEntry> Responses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Action/Adventure" },
+                new Category { CategoryId = 2, CategoryName = "Comedy" },
+                new Category { CategoryId = 3, CategoryName = "Drama" },
+                new Category { CategoryId = 4, CategoryName = "Family" },
+                new Category { CategoryId = 5, CategoryName = "Horror/Suspense" },
+                new Category { CategoryId = 6, CategoryName = "Miscellaneous" },
+                new Category { CategoryId = 7, CategoryName = "Television" },
+                new Category { CategoryId = 8, CategoryName = "VHS" }
+                );
+
+            mb.Entity<MovieEntry>().HasData(
+                new MovieEntry
+                {
+                    MovieID = 1,
+                    CategoryId = 3,
+                    Title = "High School Musical",
+                    Year = 2006,
+                    Director = "Kenny Ortega",
+                    Rating = "G",
+                },
+                new MovieEntry
+                {
+                    MovieID = 2,
+                    CategoryId = 3,
+                    Title = "tick, tick...BOOM!",
+                    Year = 2021,
+                    Director = "Lin-Manuel Miranda",
+                    Rating = "PG-13",
+                },
+                new MovieEntry
+                {
+                    MovieID = 3,
+                    CategoryId = 4,
+                    Title = "Tangled",
+                    Year = 2010,
+                    Director = "Nathan Greno",
+                    Rating = "PG",
+                }
+            );
+        }
+    }
+}
